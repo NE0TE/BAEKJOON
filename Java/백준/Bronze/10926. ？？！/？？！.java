b115abdef51a4333cdab34d7196e2e2ed5115dec
너테 NE0TE <neote.mare@gmail.com>
2025-06-23 10:34:00 +0900

    [Bronze V] Title: ??!, Time: 172 ms, Memory: 17636 KB -BaekjoonHub

---
+++ Java/백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+        String ID = in.next();
+        String Surprised = "??!";
+        System.out.print(ID+Surprised);
+	}
+}
