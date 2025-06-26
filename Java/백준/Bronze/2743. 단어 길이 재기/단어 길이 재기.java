11556c4c38e16052edfb016ff3bd9907fedf9f34
너테 NE0TE <neote.mare@gmail.com>
2025-06-26 10:31:22 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 180 ms, Memory: 17684 KB -BaekjoonHub

---
+++ Java/백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+        String S = in.nextLine();
+        System.out.print(S.length());
+	}
+}
